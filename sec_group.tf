3613f661a5b21acf117d6b772f5d96e2d869a99c
CS <CS@Chyngyzkans-MacBook-Pro.local>
2019-12-14 15:55:38 -0600

    modified

--- sec_group.tf
+++ sec_group.tf
@@ -1,8 +1,8 @@
 resource "aws_security_group" "sec_group" {
   name = "terraform-example-instance"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
